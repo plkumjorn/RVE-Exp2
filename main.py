f4d589ee656b5eabc51fdd3a55737c73391f25e7
plkumjorn <plkumjorn@gmail.com>
2017-12-30 22:31:20 +0000

    Main.py

--- main.py
+++ main.py
@@ -591,7 +591,7 @@ def translate_non_alphanumerics(to_translate, translate_to=u'_'):
 	translate_table = dict((ord(char), translate_to) for char in not_letters_or_digits)
 	return to_translate.translate(translate_table)
 
-with open("../helpers/wordsEn.txt") as word_file:
+with open("wordsEn.txt") as word_file:
 	english_words = set(word.strip().lower() for word in word_file)
 
 def is_english_word(word):
